ef4f33d4ca1b68b66be119dff581b12ff4dab5ea
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2023-10-01 11:10:14 +0300

    dom exercise tasks

--- Advanced/05-DOM Manipulations and Events/Exercise/06. Furniture/solution.js
+++ Advanced/05-DOM Manipulations and Events/Exercise/06. Furniture/solution.js
@@ -1,4 +1,45 @@
 function solve() {
 
-  //TODO...
+  const [generateBtn, buyBtn] = document.querySelectorAll('button');
+  const [inputText, outputText] = document.querySelectorAll('textarea');
+  const tableHead = document.querySelector('tbody');
+
+  generateBtn.addEventListener('click', generate);
+  buyBtn.addEventListener('click', buy);
+
+  function generate(e) {
+    const inputToObj = JSON.parse(inputText.value);
+
+    for (const obj of inputToObj) {
+      let newTr = document.createElement('tr');
+      newTr.innerHTML = `<td><img src=${obj.img}></td><td><p>${obj.name}</p></td>`;
+      newTr.innerHTML += `<td><p>${obj.price}</p></td><td><p>${obj.decFactor}</p></td>`;
+      newTr.innerHTML += '<td><input type="checkbox"/></td>';
+      tableHead.appendChild(newTr);
+    }
+  }
+
+  function buy(e) {
+    const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+    let productsArr = [];
+    let totalPrice = 0;
+    let decFactor = 0;
+    let counter = 0;
+
+    for (const check of checkBoxes) {
+      if (check.checked) {
+        const selectedItem = check.parentNode.parentNode;
+        productsArr.push(selectedItem.children[1].textContent);
+        totalPrice += Number(selectedItem.children[2].textContent);
+        decFactor += Number(selectedItem.children[3].textContent);
+        counter++;
+      }
+    }
+
+    decFactor /= counter;
+
+    outputText.value += `Bought furniture: ${productsArr.join(', ')}\n`;
+    outputText.value += `Total price: ${totalPrice.toFixed(2)}\n`;
+    outputText.value += `Average decoration factor: ${decFactor}`;
+  }
 }
--- Advanced/05-DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ Advanced/05-DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,52 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+
+    const convertBtn = document.querySelector('input[type="button"]');
+    const inputUnits = document.querySelector('#inputUnits');
+    const outputUnits = document.querySelector('#outputUnits');
+    const inputText = document.querySelector('#inputDistance');
+    const outputText = document.querySelector('#outputDistance');
+
+    convertBtn.addEventListener('click', convertToM);
+
+    function convertToM(e) {
+        let optionValue = inputUnits.value;
+        let outputValue = outputUnits.value;
+
+        if (optionValue === outputValue) {
+            return outputText.value = inputText.value;
+        }
+
+        const inputToM = {
+            'km': function (km) { return km * 1000 },
+            'm': function (m) { return m },
+            'cm': function (cm) { return cm * 0.01 },
+            'mm': function (mm) { return mm * 0.001 },
+            'mi': function (mi) { return mi * 1609.34 },
+            'yrd': function (yrd) { return yrd * 0.9144 },
+            'ft': function (ft) { return ft * 0.3048 },
+            'in': function (inches) { return inches * 0.0254 }
+        };
+
+        let convertedToM = inputToM[optionValue](Number(inputText.value));
+
+        convertToSelected(convertedToM);
+
+        function convertToSelected(convertedToM) {
+
+            const metersToOutput = {
+                'km': function (m) { return m / 1000 },
+                'm': function (m) { return m },
+                'cm': function (m) { return m / 0.01 },
+                'mm': function (m) { return m / 0.001 },
+                'mi': function (m) { return m / 1609.34 },
+                'yrd': function (m) { return m / 0.9144 },
+                'ft': function (m) { return m / 0.3048 },
+                'in': function (m) { return m / 0.0254 }
+            };
+
+            let convertedToSelected = metersToOutput[outputValue](convertedToM);
+
+            return outputText.value = convertedToSelected;
+        }
+    }
 }
