5302794e2d6582dd4beea13150f5cb347068ede9
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2023-10-09 18:07:47 +0300

    Classes Lab tasks

---
+++ Advanced/08-Classes/Lab/02-getPersons.js
@@ -0,0 +1,26 @@
+function getPersons() {
+
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+
+    let personsArr = [];
+
+    personsArr.push(new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'));
+    personsArr.push(new Person('SoftUni'));
+    personsArr.push(new Person('Stephan', 'Johnson', 25));
+    personsArr.push(new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com'));
+
+    return personsArr;
+}
+
+console.table(getPersons());
---
+++ Advanced/08-Classes/Lab/04-pointDistance.js
@@ -0,0 +1,14 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(pointA, pointB) {
+        return Math.sqrt((pointB.x - pointA.x) ** 2 + (pointB.y - pointA.y) ** 2);
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
